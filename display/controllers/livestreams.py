08f3ecb72feefc104a2abdb075145ebd3eaf4dab
Ibrahim Syah <ibrahim.syah.q@gmail.com>
2021-01-02 00:26:51 +0700

    more tweaks

--- display/controllers/livestreams.py
+++ display/controllers/livestreams.py
@@ -8,14 +8,14 @@ from datetime import datetime, timedelta
 from youtubeapi.models.video import Video
 
 def videosIndex(request):
-    upcomingVideos = Video.objects.filter(liveBroadcastContent='upcoming')
-    liveVideos = Video.objects.filter(liveBroadcastContent='live')
+    upcomingVideos = Video.objects.filter(liveBroadcastContent='upcoming').order_by('scheduledStartTime')
+    liveVideos = Video.objects.filter(liveBroadcastContent='live').order_by('actualStartTime')
 
     # still sceptical about this one, if its gte, does it also include 
     # every video in the future? of course i filtered with liveBroadcastContent='none'.
     # but still unsure nonetheless
     how_many_hours = 6
-    recentlyEndedStreams = Video.objects.filter(liveBroadcastContent='none', publishedAt__gte=datetime.now()-timedelta(hours=how_many_hours))
+    recentlyEndedStreams = Video.objects.filter(liveBroadcastContent='none', publishedAt__gte=datetime.now()-timedelta(hours=how_many_hours)).order_by('-actualStartTime')
 
     data = {
         'upcomingVideos':upcomingVideos,
