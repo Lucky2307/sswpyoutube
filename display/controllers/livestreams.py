e06be051a511e1299f64d6e49afbe2b84cd85a77
Ibrahim Syah <ibrahim.syah.q@gmail.com>
2021-01-02 01:33:28 +0700

    changed recent feed to filter the last 12 hours of streams

--- display/controllers/livestreams.py
+++ display/controllers/livestreams.py
@@ -14,7 +14,7 @@ def videosIndex(request):
     # still sceptical about this one, if its gte, does it also include 
     # every video in the future? of course i filtered with liveBroadcastContent='none'.
     # but still unsure nonetheless
-    how_many_hours = 24
+    how_many_hours = 12
     recentlyEndedStreams = Video.objects.filter(liveBroadcastContent='none', publishedAt__gte=datetime.now()-timedelta(hours=how_many_hours)).order_by('-actualStartTime')
 
     data = {
