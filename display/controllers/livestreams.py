b1d85cda8694401017d444f5a5ccb3aee70ba5e0
Ibrahim Syah <ibrahim.syah.q@gmail.com>
2021-01-03 11:39:12 +0700

    fixed feeds page no longer show unarchived videos and filtered upcoming streams far in the future

--- display/controllers/livestreams.py
+++ display/controllers/livestreams.py
@@ -8,14 +8,9 @@ from datetime import datetime, timedelta
 from youtubeapi.models.video import Video
 
 def videosIndex(request):
-    upcomingVideos = Video.objects.filter(liveBroadcastContent='upcoming').order_by('scheduledStartTime')
+    upcomingVideos = Video.objects.filter(liveBroadcastContent='upcoming', scheduledStartTime__gte=datetime.now() ,scheduledStartTime__lte=datetime.now()+timedelta(days=3)).order_by('scheduledStartTime')
     liveVideos = Video.objects.filter(liveBroadcastContent='live').order_by('actualStartTime')
-
-    # still sceptical about this one, if its gte, does it also include 
-    # every video in the future? of course i filtered with liveBroadcastContent='none'.
-    # but still unsure nonetheless
-    how_many_hours = 12
-    recentlyEndedStreams = Video.objects.filter(liveBroadcastContent='none', publishedAt__gte=datetime.now()-timedelta(hours=how_many_hours)).order_by('-actualStartTime')
+    recentlyEndedStreams = Video.objects.filter(liveBroadcastContent='none', publishedAt__gte=datetime.now()-timedelta(hours=12)).order_by('-actualStartTime')
 
     data = {
         'upcomingVideos':upcomingVideos,
